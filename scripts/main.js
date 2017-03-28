d435d901625e87b7b605aa34fa2214cc4c3551ec
Tobias Løfgren <tobloef@gmail.com>
2017-03-28 16:23:24 +0200

    Add function to add messages to DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 const maxWritingBoxHeight = 150;
 const writingBoxDefaultHeight = 37;
 
+const log = [];
+
 $(function() {
 	$(".writing-box").on("input", function() {
 		this.style.flex = `0 0 ${writingBoxDefaultHeight}px`;
@@ -9,3 +11,23 @@ $(function() {
 		this.scrollTop = this.scrollHeight;
 	});
 });
+
+function addMessage(username, message) {
+	if (getLatestChatter() !== username) {
+		$("<li />", {
+			"class": "username",
+			text: username
+		}).appendTo(".chat-log");
+	}
+	$("<li />", {
+		"class": "message",
+		text: message
+	}).appendTo(".chat-log");
+	log.push({username, message});
+}
+
+function getLatestChatter() {
+	if (log && log.length > 0) {
+		return log[log.length - 1].username;
+	}
+}
